a7262641ae0d8fa65ca05f08b3a746d8b3600870
tor1oki <253600318+tor1oki@users.noreply.github.com>
2026-01-09 03:57:20 -0600

    base
    fix 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './supplychaindemo/', // Ensures assets are loaded correctly on GitHub Pages
+  base: './', // Ensures assets are loaded correctly on GitHub Pages
   build: {
     outDir: 'dist',
     emptyOutDir: true,
